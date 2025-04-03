d954a1f71f12b5e10447bb3bb56c7db686b9dad6
Amanda <amanda.winkler@penske.com>
2025-04-03 10:51:53 -0400

    git commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { SignupUpPageComponent } from './components/signup-up-page/signup-up-page.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { authGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupUpPageComponent },
+  // will eventually use authgaurd for these
+  { path: 'noted', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+];
